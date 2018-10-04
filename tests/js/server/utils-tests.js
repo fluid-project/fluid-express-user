03c0635efa10f5b74a40edf1a485ef14907338a3
Steven Githens <swgithen@mtu.edu>
2018-10-04 12:51:16 -0700

    GPII-3255 Added remaining tests for utils
    - Tests for failure of unlocking with a bad username/password and
      creating a new account.

--- tests/js/server/utils-tests.js
+++ tests/js/server/utils-tests.js
@@ -16,6 +16,24 @@ var jqUnit       = require("node-jqunit");
 
 fluid.registerNamespace("gpii.tests.express.user.utils.caseHolder");
 
+gpii.tests.express.user.utils.createUser = function (utils, sequenceDelay) {
+    var prom = utils.createNewUser({
+        username: "myFirstUser",
+        password: "this is a password",
+        email: "myFirstUser@gpii.net"
+    });
+    prom.then(function (data) {
+        jqUnit.assertEquals("Generated Couch ID", "org.couch.db.user:myFirstUser", data._id);
+        jqUnit.assertEquals("Email", "myFirstUser@gpii.net", data.email);
+        jqUnit.assertEquals("Username", "myFirstUser", data.username);
+        jqUnit.assertTrue("Unlock the password", gpii.express.user.utils.verifyPassword(data, "this is a password"));
+        sequenceDelay.events.onComplete.fire();
+    }, function (err) {
+        jqUnit.fail("Unable to create user with error: " + err);
+        sequenceDelay.events.onComplete.fire();
+    });
+};
+
 gpii.tests.express.user.utils.unlockUsernamePassword = function (utils, sequenceDelay) {
     var prom = utils.unlockUser("existing", "password");
     prom.then(function (data) {
@@ -23,6 +41,17 @@ gpii.tests.express.user.utils.unlockUsernamePassword = function (utils, sequence
         sequenceDelay.events.onComplete.fire();
     }, function (err) {
         jqUnit.fail("Error unlocking user: " + JSON.stringify(err));
+        sequenceDelay.events.onComplete.fire();
+    });
+};
+
+gpii.tests.express.user.utils.incorrectUsernamePassword = function (utils, sequenceDelay) {
+    var prom = utils.unlockUser("not-existing", "password");
+    prom.then(function () {
+        jqUnit.fail("This user shouldn't have unlocked with the supplied credentials.");
+        sequenceDelay.events.onComplete.fire();
+    }, function () {
+        jqUnit.assert("Succeeded in not unlocking with incorrect credentials");
         sequenceDelay.events.onComplete.fire();
     });
 };
@@ -38,7 +67,8 @@ fluid.defaults("gpii.tests.express.user.sequenceDelay", {
     }
 });
 
-// Each test has a request instance of `kettle.test.request.http` or `kettle.test.request.httpCookie`, and a test module that wires the request to the listener that handles its results.
+// Each test has a request instance of `kettle.test.request.http` or `kettle.test.request.httpCookie`,
+// and a test module that wires the request to the listener that handles its results.
 fluid.defaults("gpii.tests.express.user.utils.caseHolder", {
     gradeNames: ["gpii.test.webdriver.caseHolder"],
     components: {
@@ -51,7 +81,22 @@ fluid.defaults("gpii.tests.express.user.utils.caseHolder", {
             name: "Testing login functions...",
             tests: [
                 {
-                    name: "Testing logging in with an unverified account...",
+                    name: "Create a new user and verify their data.",
+                    type: "test",
+                    sequence: [
+                        {
+                            funcName: "gpii.tests.express.user.utils.createUser",
+                            args: ["{gpii.express.user.utils}", "{sequenceDelay}"]
+                        },
+                        {
+                            listener: "fluid.log",
+                            event: "{sequenceDelay}.events.onComplete",
+                            args: ["Finished create user test"]
+                        }
+                    ]
+                },
+                {
+                    name: "Testing unlocking a user with correct credentials.",
                     type: "test",
                     sequence: [
                         {
@@ -64,7 +109,23 @@ fluid.defaults("gpii.tests.express.user.utils.caseHolder", {
                             args: ["Finished unlockUsernamePassword test"]
                         }
                     ]
+                },
+                {
+                    name: "Testing not unlocking a user with incorrect credentials.",
+                    type: "test",
+                    sequence: [
+                        {
+                            funcName: "gpii.tests.express.user.utils.incorrectUsernamePassword",
+                            args: ["{gpii.express.user.utils}", "{sequenceDelay}"]
+                        },
+                        {
+                            listener: "fluid.log",
+                            event: "{sequenceDelay}.events.onComplete",
+                            args: ["Finished incorrect credentials test"]
+                        }
+                    ]
                 }
+
             ]
         }
     ]
